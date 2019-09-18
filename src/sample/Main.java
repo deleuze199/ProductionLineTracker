fc83fe18a30552d301eb9e7713d594550a006293
deleuze199 <deleuze199@gmail.com>
2019-09-18 19:00:21 -0400

    added buttons in each tab and grip plain in first tab

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Benjamin Deleuze
+ * @brief
+ */
 public class Main extends Application {
+  /**
+   * Start of JavaFX program
+   *
+   * @param primaryStage first thing the user sees
+   * @throws Exception
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 350, 300));
+    primaryStage.show();
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }

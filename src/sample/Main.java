32a2e62ce9945dc315a98fc59bc17335feb22c9f
deleuze199 <deleuze199@gmail.com>
2019-09-19 20:56:18 -0400

    Add a Label and ListView for Choose Product and add a Label and ComboBox for Choose Quantity. Need to event handle button push and add css file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
     primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 350, 300));
+    primaryStage.setScene(new Scene(root, 350, 345));
     primaryStage.show();
   }
 

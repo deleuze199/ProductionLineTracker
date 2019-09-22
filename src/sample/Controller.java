b722de47693378e0590980968d20ae775396faa5
deleuze199 <deleuze199@gmail.com>
2019-09-21 21:14:53 -0400

    completed: google style, inspect, fig bugs Needs work: css file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,49 +1,85 @@
 package sample;
 
-
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 
-import java.net.URL;
-import java.sql.*;
-import java.util.ResourceBundle;
 
+
+
+/**
+ * This is the Controller Class for the UI part of the program.
+ *
+ * @author Benjamin Deleuze
+ * @version a.1.0 9/21/2019
+ */
 public class Controller implements Initializable {
-    @FXML private ComboBox comboBox;
-    @FXML private ChoiceBox choiceBox;
+  @FXML private ComboBox comboBox;
+  @FXML private ChoiceBox choiceBox;
 
-    public void productLineButtonHandler() {
-
-        final String JDBC_DRIVER = "org.h2.Driver";
-        final String DB_URL = "jdbc:h2:./res/h2";
-        final String USER = "";
-        final String PASS = "";
-        Connection conn;
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            String insertProductLine = "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
-            PreparedStatement preparedstmt = conn.prepareStatement(insertProductLine);
-            preparedstmt.execute();
-            conn.close();
-            preparedstmt.close();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Product Line Button");
-
+  /**
+  * productLineButtonHandler method is a handler for when the "Add Product" button is click.
+  * This method adds the product to the database.
+  */
+  public void productLineButtonHandler() {
+    // Database driver and location
+    final String Jdbc_Driver = "org.h2.Driver";
+    final String db_Url = "jdbc:h2:./res/h2";
+    //Database credentials
+    final String user = "";
+    final String pass = "";
+    //instance of class connection
+    Connection conn;
+    try {
+      //Register JDBC driver
+      Class.forName(Jdbc_Driver);
+      //Create a connection to database
+      conn = DriverManager.getConnection(db_Url, user, pass);
+      //SQL String to add a product to the database
+      String insertProductLine =
+          "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+      //Object to add SQL String as argument to PreparedStatement
+      PreparedStatement preparedstmt = conn.prepareStatement(insertProductLine);
+      //Execute SQL string
+      preparedstmt.execute();
+      conn.close();
+      preparedstmt.close();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
     }
+    System.out.println("Product Line Button");
 
-    public void produceBtnHandler() {
-        System.out.println("Produce Button");
-    }
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        choiceBox.getItems().addAll("Audio","Video","Other");
-        comboBox.getItems().addAll("1","2","3","4","5","6","7","8","9","10");
-        comboBox.getSelectionModel().selectFirst();
-        comboBox.setEditable(true);
-    }
+  }
+
+  /**
+  * produceBtnHandler method is a handler for when the "Record Production" button is clicked.
+  * This method for now just prints to the console "Produce Button".
+  */
+  public void produceBtnHandler() {
+    System.out.println("Produce Button");
+  }
+
+  /**
+   * The initialize method populates numbers 1-10 in the ComboBox and
+   * populates "Audio","Video","Other" in the ChoiceBox.
+   * @param location is the location of the database.
+   * @param resources is the reference to the resources.
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    //populate "Audio","Video","Other" in the ChoiceBox
+    choiceBox.getItems().addAll("Audio","Video","Other");
+    //populate numbers 1-10 in the ComboBox
+    comboBox.getItems().addAll("1","2","3","4","5","6","7","8","9","10");
+    comboBox.getSelectionModel().selectFirst();
+    //makes ComboBox editable
+    comboBox.setEditable(true);
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,62 +1,64 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.sql.*;
-
 /**
  * This is a assembly production tracker.
  *
  * @author Benjamin Deleuze
- * @version a.1.0
+ * @version a.1.0 9/21/2019
  */
 public class Main extends Application {
 
-    /**
-     * Start of JavaFX program.
-     *
-     * @param primaryStage first thing the user sees.
-     * @throws Exception for any issue.
-     */
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Production Line Tracker");
-        primaryStage.setScene(new Scene(root, 350, 345));
-        primaryStage.show();
+  /**
+  * Start of JavaFX program.
+  *
+  * @param primaryStage first thing the user sees.
+  * @throws Exception for any issue.
+  */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Production Line Tracker");
+    primaryStage.setScene(new Scene(root, 350, 345));
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    initializeDB();
+    launch(args);
+  }
+
+  /**
+  * This method initializes the database and connects to it.
+  */
+  private static void initializeDB() {
+    final String Jdbc_Driver = "org.h2.Driver";
+    final String db_Url = "jdbc:h2:./res/h2";
+    Statement stmt;
+    // Database credentials
+    final String user = ""; //dont public database credentials to git
+    final String pass = "";
+    Connection conn; //instance of class connection
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(Jdbc_Driver);
+      //Create a connection to database
+      conn = DriverManager.getConnection(db_Url, user, pass);
+      //Use the connection to create a statement
+      stmt = conn.createStatement();
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
     }
-
-    public static void main(String[] args) {
-        initializeDB();
-        launch(args);
-    }
-
-    private static void initializeDB() {
-        final String JDBC_DRIVER = "org.h2.Driver";
-        final String DB_URL = "jdbc:h2:./res/h2";
-
-        Statement stmt;
-        //  Database credentials
-        final String USER = ""; //dont public database credentials to git
-        final String PASS = "";
-        Connection conn; //instance of class connection set to null
-        //Statement stmt = null; //instance of class Statement set to null
-        try {
-            // STEP 1: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-            //Create a connection to database
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            //Use the connection to create a statement
-            stmt = conn.createStatement();
-            stmt.close();
-            conn.close();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+  }
 }

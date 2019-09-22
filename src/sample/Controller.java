6a59e70b98a2920a657f1a89305126ca2d48fdff
deleuze199 <deleuze199@gmail.com>
2019-09-21 20:08:46 -0400

    completed: event handling for button push, added options to combobox and choicebox Needs work: css file, fig bugs, google style, inspect

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,49 @@
 package sample;
 
-public class Controller {
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML private ComboBox comboBox;
+    @FXML private ChoiceBox choiceBox;
+
+    public void productLineButtonHandler() {
+
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/h2";
+        final String USER = "";
+        final String PASS = "";
+        Connection conn;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            String insertProductLine = "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+            PreparedStatement preparedstmt = conn.prepareStatement(insertProductLine);
+            preparedstmt.execute();
+            conn.close();
+            preparedstmt.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Product Line Button");
+
+    }
+
+    public void produceBtnHandler() {
+        System.out.println("Produce Button");
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choiceBox.getItems().addAll("Audio","Video","Other");
+        comboBox.getItems().addAll("1","2","3","4","5","6","7","8","9","10");
+        comboBox.getSelectionModel().selectFirst();
+        comboBox.setEditable(true);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,27 +6,57 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 /**
  * This is a assembly production tracker.
+ *
  * @author Benjamin Deleuze
  * @version a.1.0
  */
 public class Main extends Application {
-  /**
-   * Start of JavaFX program.
-   *
-   * @param primaryStage first thing the user sees.
-   * @throws Exception for any issue.
-   */
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 350, 345));
-    primaryStage.show();
-  }
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    /**
+     * Start of JavaFX program.
+     *
+     * @param primaryStage first thing the user sees.
+     * @throws Exception for any issue.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Production Line Tracker");
+        primaryStage.setScene(new Scene(root, 350, 345));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        initializeDB();
+        launch(args);
+    }
+
+    private static void initializeDB() {
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/h2";
+
+        Statement stmt;
+        //  Database credentials
+        final String USER = ""; //dont public database credentials to git
+        final String PASS = "";
+        Connection conn; //instance of class connection set to null
+        //Statement stmt = null; //instance of class Statement set to null
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            //Create a connection to database
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            //Use the connection to create a statement
+            stmt = conn.createStatement();
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }

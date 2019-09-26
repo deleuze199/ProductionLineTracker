7a0ec9aca7057ab85730bb46f61cf1e0b097f126
deleuze199 <deleuze199@gmail.com>
2019-09-25 20:58:26 -0400

    corrected google style

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -58,7 +58,7 @@ public class Controller implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     // populate "Audio","Video","Other" in the ChoiceBox
-    choiceBox.getItems().addAll("Audio", "Video", "Other");
+    choiceBox.getItems().addAll("Audio", "Visual", "AudioMobile","VisualMobile");
     // populate numbers 1-10 in the ComboBox
     comboBox.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
     comboBox.getSelectionModel().selectFirst();
